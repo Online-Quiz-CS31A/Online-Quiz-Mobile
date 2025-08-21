04ae101374e0835920f01ceed01d173338ae42be
John Cez Casupanan <casupananjohncez@gmail.com>
2025-08-21 21:37:44 +0800

    feat: add models for Course, NotificationItem, Quiz, QuizResult, and User

---
+++ online_quiz/lib/models/course.dart
@@ -0,0 +1,19 @@
+import 'quiz.dart';
+
+class Course {
+  final String id;
+  final String name;
+  final String code;
+  final List<Quiz> quizzes;
+  final String instructor;
+  final int credits;
+
+  Course({
+    required this.id,
+    required this.name,
+    required this.code,
+    required this.quizzes,
+    required this.instructor,
+    required this.credits,
+  });
+}
